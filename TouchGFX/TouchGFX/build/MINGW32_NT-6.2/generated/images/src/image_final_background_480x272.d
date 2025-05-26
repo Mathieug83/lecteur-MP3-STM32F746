1693531b71a489cfe1bda0d727d720ba368c20d2
Mathieug83 <147141812+Mathieug83@users.noreply.github.com>
2025-05-26 14:35:31 -0400

    Delete TouchGFX/TouchGFX directory

--- TouchGFX/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/image_final_background_480x272.d
+++
@@ -1,5 +0,0 @@
-build/MINGW32_NT-6.2/generated/images/src/image_final_background_480x272.o: \
- generated/images/src/image_final_background_480x272.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
